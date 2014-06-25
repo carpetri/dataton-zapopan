409bf0a13e615affd4c4a1466b297cae864bfec9
Carlos Petricioli <carpetri@gmail.com>
2014-06-25 18:39:50 -0500

    Primera gráfica de delitos

--- src/00_datos_delitos.R
+++ src/00_datos_delitos.R
@@ -8,11 +8,16 @@ ls()
 #GEOESPACIAL
 nclr <- 9
 plotclr <- brewer.pal(nclr,"YlOrRd")
-jalisco <- readShapeSpatial("data/inegi-jalisco/jal_ageb_urb.shp")
-jalisco.fuerte <- fortify(jalisco)
-head(jalisco.fuerte)
+agebs.jalisco <- readShapeSpatial("data/inegi-jalisco/jal_ageb_urb.shp")
+agebs.jalisco.fuerte <- fortify(agebs.jalisco)
+head(agebs.jalisco.fuerte)
 
-head(delitos)
+mun.jalisco <- readShapeSpatial("data/inegi-jalisco/jal_municipal.shp")
+mun.jalisco.fuerte <- fortify(mun.jalisco)
+
+head(mun.jalisco.fuerte)
+
+
 delitos.1 <- filter( delitos, x >0)
 
 
@@ -23,6 +28,17 @@ ggplot(data = jalisco.fuerte, aes(long, lat)) +
        colour = "Tipo de delito", size = "", alpha = "")  + 
    geom_point(data = delitos.1, aes(x = long, y = lat, colour = tipo),
               size = 1.5, alpha = 0.9) +   scale_colour_brewer(palette="YlOrRd") 
+
+
+
+ggplot(data = mun.jalisco.fuerte, aes(long, lat)) + 
+  geom_polygon(colour='darkgray', fill='white', aes(group=group)) + 
+  coord_fixed(ratio = 5/3.5)+  labs(title = "Municipios en Jalisco", x = "Longitud", y = "Latitud", 
+                                    colour = "Tipo de delito", size = "", alpha = "")  + 
+  geom_point(data = delitos.1, aes(x = long, y = lat, colour = tipo),
+             size = 1.5, alpha = 0.9) +   scale_colour_brewer(palette="YlOrRd") 
+ggsave(filename = 'munucipios_y_delitos_jalisco.pdf', width = 9, height = 3 )
+
 
 
 
