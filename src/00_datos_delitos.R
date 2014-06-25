71f45ed3530c921b62793679cd25f234c99da113
Carlos Petricioli <carpetri@gmail.com>
2014-06-25 11:22:10 -0500

    Primer plot de los delitos en los agebs de Jalisco

--- src/00_datos_delitos.R
+++ src/00_datos_delitos.R
@@ -12,16 +12,16 @@ jalisco <- readShapeSpatial("data/inegi-jalisco/jal_ageb_urb.shp")
 jalisco.fuerte <- fortify(jalisco)
 head(jalisco.fuerte)
 
+head(delitos)
+
 
 
 ggplot(data = jalisco.fuerte, aes(long, lat)) + 
   geom_polygon(colour='darkgray', fill='white', aes(group=group)) + 
   coord_fixed(ratio = 5/3.5)+  labs(title = "AGEBS en Jalisco", x = "Longitud", y = "Latitud", 
-       colour = "Residuales", size = "", alpha = "") 
-# + 
-#   geom_point(data = delitos, aes(x = Longitude, y = Latitude, colour = res.c),
-#              size = 3.5, alpha = 0.9) +
-#   scale_colour_brewer(palette="YlOrRd") 
+       colour = "Residuales", size = "", alpha = "")  + 
+   geom_point(data = delitos, aes(x = long, y = lat, colour = fecha),
+              size = 3.5, alpha = 0.9) +   scale_colour_brewer(palette="YlOrRd") 
 
 
 
