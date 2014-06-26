61b7a82d0ab1b9c376c7008fb05bebdbc97de324
Carlos Petricioli <carpetri@gmail.com>
2014-06-26 08:43:59 -0500

    Lo puse en la carpeta correcta

--- src/01_coordenadas_por_colonia.R
+++ src/01_coordenadas_por_colonia.R
@@ -8,3 +8,9 @@ delitos.detenidos.1 <- filter(delitos.detenidos, x>0 )
 delitos.detenidos.1$tipo <- gsub(delitos.detenidos.1$tipo, pattern = 'FALTAS ADMINISTRATIVAS', 
                                  replacement = 'FALTA ADMINISTRATIVA')
 
+
+coord.colonias <- delitos.1 %.% group_by(colonia) %.% summarize( long= mean(long), lat=mean(lat),
+                                                                 n=n())
+
+
+write.csv(coord.colonias,'data/coordenas_por_colonia.csv')
